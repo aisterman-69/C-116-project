3a447345f85cda65beec63acd1ba2412fdc16404
aisterman-69 <77432759+aisterman-69@users.noreply.github.com>
2023-01-24 20:26:28 +1100

    Add files via upload

---
+++ PRO-C116-Project-Boilerplate-main/PRO-C116-Project-Boilerplate-main/family tree/app.py
@@ -0,0 +1,42 @@
+# importing modules from flask library
+from flask import Flask , render_template
+
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "Aiden" # write your name
+    age = "14" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/father")
+def father_webpage():
+    name = "Dad"
+    age = '45'
+    return render_template("father.html", name = name, age = age)
+
+# define the route to mother webpage
+@app.route("/mother")
+def mother_webpage():
+    name = "Mom"
+    age = '40'
+    return render_template("mother.html", name = name , age = age)
+
+# define the route to friends webpage
+@app.route("/friend")
+def friend_webpage():
+    name = "Alex"
+    age = '14'
+    return render_template("friend.html", name = name, age = age)
+
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
